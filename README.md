cf2eb30bf3bd19d87dd25d3996c8a0683813aec5
Kaixhin <design@kaixhin.com>
2015-12-22 17:49:20 +0000

    Tidy up alewrap environment

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Atari
-DQN-based reinforcement learning agent
+Atari
+=====
+
+Arcade Learning Environment made more accessible for Torch7.
+
+Requirements
+------------
+
+Requires [Torch7](http://torch.ch/) with CUDA support and the following packages:
+
+- cudnn
+- moses
+- xitari
+- alewrap
+
+xitari and alewrap can be installed using the following commands:
+
+```sh
+luarocks install https://raw.githubusercontent.com/Kaixhin/xitari/master/xitari-0-0.rockspec
+luarocks install https://raw.githubusercontent.com/Kaixhin/alewrap/master/alewrap-0-0.rockspec
+```
--- environment.lua
+++ environment.lua
@@ -3,8 +3,22 @@ local framework = require 'alewrap'
 local environment = {}
 
 -- Initialises ALE with game
-environment.init = function(game)
-  return framework.GameEnvironment({game_path="roms", env=game})
+environment.init = function(opt)
+  -- Set GPU flag (GPU enables faster screen buffer)
+  local gpu = opt.gpu - 1
+  -- Create options from opt
+  local options = {
+    game_path = "roms",
+    env = opt.game,
+    actrep = opt.actrep,
+    random_starts = opt.random_starts,
+    gpu = gpu,
+    pool_frms = {
+      size = 2, -- Pool over frames to prevent problems with fixed interval events
+      type = 'max' -- Defaults to 2-frame mean-pooling
+    }
+  }
+  return framework.GameEnvironment(options)
 end
 
 return environment
