e84540b72a7f0f75ceb4dd00606715156130b902
lake4790k <lake4790k@users.noreply.github.com>
2016-05-08 09:52:33 +0200

    add OMP_NUM_THREADS=1

--- async_run.sh
+++ async_run.sh
@@ -28,12 +28,14 @@ if [ "$PAPER" != "demo" ]; then
   fi
 fi
 
+export OMP_NUM_THREADS=1 
+
 if [ "$PAPER" == "demo" ]; then
   # Catch demo
   th async_main.lua -async $ASYNC -eta 0.00025 -doubleQ false -duel false -optimiser adam -steps 15000000 -tau 4 -epsilonSteps 10000 -valFreq 10000 -valSteps 6000 -PALpha 0 "$@"
 elif [ "$PAPER" == "nature" ]; then
   # Nature
-    th async_main.lua -async $ASYNC -game $GAME -duel false -tau 40000 -optimiser sharedRmsProp -epsilonSteps 4000000 -doubleQ false -PALpha 0 -eta 0.0016 -gradClip 0 "$@"
+  th async_main.lua -async $ASYNC -game $GAME -duel false -tau 40000 -optimiser sharedRmsProp -epsilonSteps 4000000 -doubleQ false -PALpha 0 -eta 0.0016 -gradClip 0 "$@"
 elif [ "$PAPER" == "doubleq" ]; then
   # Double-Q (tuned)
   th async_main.lua -async $ASYNC -game $GAME -duel false -PALpha 0 -eta 0.00025 -gradClip 0 "$@"
