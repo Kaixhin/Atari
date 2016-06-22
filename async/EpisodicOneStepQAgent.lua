db7e09330e9e748cc38144e9efe5c7bc5a724c28
lake4790k <lake4790k@users.noreply.github.com>
2016-06-22 19:01:13 +0200

    Clone states for memory

--- async/EpisodicOneStepQAgent.lua
+++ async/EpisodicOneStepQAgent.lua
@@ -28,6 +28,9 @@ function EpisodicOneStepQAgent:learn(steps, from)
 
   local action, state_
 
+  self.batchIdx = 1
+  self.states[self.batchIdx] = state:clone()
+
   self.tic = torch.tic()
   for step1=1,steps do
     if not terminal then
@@ -39,11 +42,10 @@ function EpisodicOneStepQAgent:learn(steps, from)
 
     self.batchIdx = self.batchIdx + 1
 
-    self.states[self.batchIdx] = state_
+    self.states[self.batchIdx] = state_:clone()
     self.actions[self.batchIdx] = action
     self.rewards[self.batchIdx] = reward
-    self.rewards[self.batchIdx] = reward
-    self.Qs[self.batchIdx] = self.QCurr
+    self.Qs[self.batchIdx] = self.QCurr:clone()
 
     if not terminal then
       state = state_
@@ -65,6 +67,7 @@ end
 function EpisodicOneStepQAgent:learnEpisode(steps)
 --  log.info('learning episode length=%d', #self.rewards)
 local suc = self.rewards[self.batchIdx]  > 0
+
   for i=2,#self.rewards do
     local terminal = i == self.batchIdx
     local state = self.states[i-1]
@@ -72,18 +75,17 @@ local suc = self.rewards[self.batchIdx]  > 0
     local action = self.actions[i]
     local reward = self.rewards[i]
 
-    self:accumulateGradient(state, action, state_, reward, terminal)
-
     local tdErrQ = self:computeTdErrQ(state, action, state_, reward, terminal)
 
     local totalReward = 0
     local gamma = 1
-    for r=i,#self.rewards do
-      totalReward = gamma * self.rewards[r]
-      gamma = gamma * self.gamma
-      if gamma < 0.001 then break end
+    if self.eta < 1 then
+      for r=i,#self.rewards do
+        totalReward = gamma * self.rewards[r]
+        gamma = gamma * self.gamma
+        if gamma < 0.001 then break end
+      end
     end
-
     local mcReturn = totalReward - self.Qs[i][action]
 
     local mixedDelta = self.eta * tdErrQ + (1- self.eta) * mcReturn
@@ -94,6 +96,7 @@ end
     self:accumulateGradientTdErr(state, action, mixedDelta, self.policyNet)
 
     if i % self.batchSize == 0 or terminal then
+
       self:applyGradients(self.policyNet, self.dTheta, self.theta)
     end
     self:progress(steps)
