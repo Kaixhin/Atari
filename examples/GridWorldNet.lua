dc0bcc929ff9b78fcfbe549a03e77385df1d8c09
Mr-Yellow <mr-yellow@mr-yellow.com>
2016-08-14 07:21:45 +1000

    Add documentation and example model see #51

--- examples/GridWorldNet.lua
+++ examples/GridWorldNet.lua
@@ -1,8 +1,29 @@
 local nn = require 'nn'
+local nninit = require 'nninit'
+require 'classic.torch' -- Enables serialisation
+--require 'rnn'
+--require 'dpnn' -- Adds gradParamClip method
 
-local net = nn.Sequential()
-net:add(nn.View(2))
-net:add(nn.Linear(2, 32))
-net:add(nn.ReLU(true))
+local Body = classic.class('Body')
 
-return net
+-- Constructor
+function Body:_init(opts)
+  opts = opts or {}
+
+  --self.recurrent = opts.recurrent
+  --self.histLen = opts.histLen
+  --self.stateSpec = opts.stateSpec
+end
+
+function Body:createBody()
+  local net
+
+  net = nn.Sequential()
+  net:add(nn.View(2))
+  net:add(nn.Linear(2, 32))
+  net:add(nn.ReLU(true))
+
+  return net
+end
+
+return Body
